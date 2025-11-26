4434db2dec35b342d13b1447632597655c31aad7
Ayhan <90261544+ayhandeveci@users.noreply.github.com>
2025-11-26 12:40:02 +0300

    Modify password comment and extend image list
    Updated password comment and added new image file names.

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const PASSWORD = "1234";   // ŞİFRE — değiştirilebilir
+const PASSWORD = "1234";   // Şifreyi buradan değiştir
 
 function login() {
     const entered = document.getElementById("pwd").value;
@@ -14,13 +14,111 @@ function login() {
     loadImages();
 }
 
-// PNG dosyalarını burada listeleyeceksin
 function loadImages() {
 
     const images = [
-        // ÖRNEK — kendi PNG isimlerinle değiştireceksin
-        "Finans_2014_1.png",
-        "Finans_2014_2.png"
+
+"2013_Aralik_1.png",
+"2013_Aralik_2.png",
+"2013_Aralik_3.png",
+"2013_Aralik_4.png",
+"2013_Aralik_5.png",
+"2013_Aralik_6.png",
+"2013_Aralik_7.png",
+"2013_Aralik_8.png",
+
+"2014_Aralik_1.png",
+"2014_Aralik_2.png",
+"2014_Aralik_3.png",
+"2014_Aralik_4.png",
+"2014_Aralik_5.png",
+"2014_Aralik_6.png",
+"2014_Aralik_7.png",
+
+"2015_Aralik_1.png",
+"2015_Aralik_2.png",
+"2015_Aralik_3.png",
+"2015_Aralik_4.png",
+"2015_Aralik_5.png",
+"2015_Aralik_6.png",
+"2015_Aralik_7.png",
+
+"2016_Aralik_1.png",
+"2016_Aralik_2.png",
+"2016_Aralik_3.png",
+"2016_Aralik_4.png",
+"2016_Aralik_5.png",
+"2016_Aralik_6.png",
+"2016_Aralik_7.png",
+
+"2017_Aralik_1.png",
+"2017_Aralik_2.png",
+"2017_Aralik_3.png",
+"2017_Aralik_5.png",
+"2017_Aralik_6.png",
+"2017_Aralik_7.png",
+
+"2018_Aralik_1.png",
+"2018_Aralik_2.png",
+"2018_Aralik_3.png",
+"2018_Aralik_4.png",
+"2018_Aralik_5.png",
+"2018_Aralik_6.png",
+"2018_Aralik_7.png",
+
+"2019_Aralik_1.png",
+"2019_Aralik_2.png",
+"2019_Aralik_3.png",
+"2019_Aralik_4.png",
+"2019_Aralik_5.png",
+"2019_Aralik_6.png",
+"2019_Aralik_7.png",
+
+"2021_Kasim_1.png",
+"2021_Kasim_2.png",
+"2021_Kasim_3.png",
+"2021_Kasim_4.png",
+"2021_Kasim_5.png",
+"2021_Kasim_6.png",
+"2021_Kasim_7.png",
+
+"2021_Mayis_1.png",
+"2021_Mayis_2.png",
+"2021_Mayis_3.png",
+"2021_Mayis_4.png",
+"2021_Mayis_5.png",
+"2021_Mayis_6.png",
+"2021_Mayis_7.png",
+
+"2022_Aralik_1.png",
+"2022_Aralik_2.png",
+"2022_Aralik_3.png",
+"2022_Aralik_4.png",
+"2022_Aralik_5.png",
+"2022_Aralik_6.png",
+"2022_Aralik_7.png",
+
+"2023_Aralik_2.png",
+"2023_Aralik_5.png",
+"2023_Aralik_7.png",
+"2023_Aralik_9.png",
+"2023_Aralik_12.png",
+"2023_Aralik_13.png",
+"2023_Aralik_15.png",
+
+"2024_Aralik_1.png",
+"2024_Aralik_2.png",
+"2024_Aralik_3.png",
+"2024_Aralik_4.png",
+"2024_Aralik_5.png",
+"2024_Aralik_6.png",
+"2024_Aralik_7.png",
+"2024_Aralik_8.png",
+"2024_Aralik_9.png",
+"2024_Aralik_11.png",
+"2024_Aralik_13.png",
+"2024_Aralik_15.png"
+
     ];
 
     const container = document.getElementById("questions");
